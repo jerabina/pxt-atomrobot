9ecc982f0be598d2e1352381357eb574fa2d3d39
Luboš Jeřábek <jerabina@users.noreply.github.com>
2022-02-06 20:04:45 +0100

    Aktualizovat neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -1,9 +1,13 @@
 // Functions to operate NeoPixel strips.
 
 enum NeoPixelLed {
+    //% block=led 0
     LED0 = 0,
+    //% block=led 1
     LED1 = 1,
+    //% block=led 2
     LED2 = 2,
+    //% block=led 3
     LED3 = 3,
 }
 
