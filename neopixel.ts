9dc80c6e88aa7b21e985c32326fb61490376f37c
Luboš Jeřábek <jerabina@users.noreply.github.com>
2022-02-06 19:52:34 +0100

    Aktualizovat neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -1,42 +1,46 @@
 // Functions to operate NeoPixel strips.
 
+
+enum NeoPixelColors {
+    //% block=red
+    Red = 0xFF0000,
+    //% block=orange
+    Orange = 0xFFA500,
+    //% block=yellow
+    Yellow = 0xFFFF00,
+    //% block=green
+    Green = 0x00FF00,
+    //% block=blue
+    Blue = 0x0000FF,
+    //% block=indigo
+    Indigo = 0x4b0082,
+    //% block=violet
+    Violet = 0x8a2be2,
+    //% block=purple
+    Purple = 0xFF00FF,
+    //% block=white
+    White = 0xFFFFFF,
+    //% block=black
+    Black = 0x000000
+}
+
+/**
+ * Different modes for RGB or RGB+W NeoPixel strips
+ */
+enum NeoPixelMode {
+    //% block="RGB (GRB format)"
+    RGB = 0,
+    //% block="RGB+W"
+    RGBW = 1,
+    //% block="RGB (RGB format)"
+    RGB_RGB = 2
+}
+
 //% color="#4d00b5" icon="\uf135" block="Robot ATOM"
 //% category="Robot ATOM"
 namespace robotAtom {
-    enum NeoPixelColors {
-        //% block=red
-        Red = 0xFF0000,
-        //% block=orange
-        Orange = 0xFFA500,
-        //% block=yellow
-        Yellow = 0xFFFF00,
-        //% block=green
-        Green = 0x00FF00,
-        //% block=blue
-        Blue = 0x0000FF,
-        //% block=indigo
-        Indigo = 0x4b0082,
-        //% block=violet
-        Violet = 0x8a2be2,
-        //% block=purple
-        Purple = 0xFF00FF,
-        //% block=white
-        White = 0xFFFFFF,
-        //% block=black
-        Black = 0x000000
-    }
 
-    /**
-     * Different modes for RGB or RGB+W NeoPixel strips
-     */
-    enum NeoPixelMode {
-        //% block="RGB (GRB format)"
-        RGB = 0,
-        //% block="RGB+W"
-        RGBW = 1,
-        //% block="RGB (RGB format)"
-        RGB_RGB = 2
-    }
+
 
     //% shim=sendBufferAsm
     function sendBuffer(buf: Buffer, pin: DigitalPin) {
